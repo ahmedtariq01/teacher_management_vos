2af8b736c95a7b934c83d31d06fa9606fd1b1e25
Ahmed Tariq <ahmed.tm097@gmail.com>
2024-12-20 12:00:00 +0500

    Commit 2

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set the JavaFX library path
+JAVAFX_LIB="/home/ahmedtm/Documents/java_work/Project/src/lib"
+
+# Compile the Java files
+javac --module-path $JAVAFX_LIB --add-modules javafx.controls,javafx.fxml \
+      -d /home/ahmedtm/Documents/java_work/Project/out \
+      /home/ahmedtm/Documents/java_work/Project/src/main/*.java \
+      /home/ahmedtm/Documents/java_work/Project/src/fx/*.java \
+      /home/ahmedtm/Documents/java_work/Project/src/utils/*.java
+
+# Run the application
+java --module-path $JAVAFX_LIB --add-modules javafx.controls,javafx.fxml \
+     -cp /home/ahmedtm/Documents/java_work/Project/out \
+     main.loadmanagementsystem
+// Commit 2
